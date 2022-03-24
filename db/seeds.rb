72f9fb0cd190fc861859442af3fa1739b40b6d72
craig cardwell <wizeduvcc@gmail.com>
2022-03-24 15:12:11 -0600

    edit seeds file to add teams instances with relation to leagues

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Team.destroy_all
 League.destroy_all
 
-@league_1 = League.create!(name: 'league1', professional: true, years_in_existence: 5)
-@league_2 = League.create!(name: 'league2', professional: false, years_in_existence: 10)
-@league_3 = League.create!(name: 'league3', professional: false, years_in_existence: 7)
-@league_4 = League.create!(name: 'league4', professional: true, years_in_existence: 3)
+league_1 = League.create!(name: 'league1', professional: true, years_in_existence: 5)
+league_2 = League.create!(name: 'league2', professional: false, years_in_existence: 10)
+league_3 = League.create!(name: 'league3', professional: false, years_in_existence: 7)
+league_4 = League.create!(name: 'league4', professional: true, years_in_existence: 3)
+
+
+league_1.teams.create!(name: 'team1', expansion_team: true, number_of_players: 10)
+league_1.teams.create!(name: 'team2', expansion_team: false, number_of_players: 15)
+league_1.teams.create!(name: 'team3', expansion_team: true, number_of_players: 12)
+
+league_2.teams.create!(name: 'team4', expansion_team: false, number_of_players: 53)
+league_2.teams.create!(name: 'team5', expansion_team: false, number_of_players: 19)
+league_2.teams.create!(name: 'team6', expansion_team: true, number_of_players: 25)
+
+league_3.teams.create!(name: 'team7', expansion_team: true, number_of_players: 7)
+league_3.teams.create!(name: 'team8', expansion_team: true, number_of_players: 33)
+league_3.teams.create!(name: 'team9', expansion_team: false, number_of_players: 16)
+
+league_4.teams.create!(name: 'team10', expansion_team: false, number_of_players: 27)
+league_4.teams.create!(name: 'team11', expansion_team: true, number_of_players: 40)
+league_4.teams.create!(name: 'team12', expansion_team: true, number_of_players: 9)
